3d462500e3e25bb2bd6d4245f984a4f248ddac8c
DanielTTrading <dv@ttrading.co>
2025-09-10 18:35:09 -0500

    ids

--- app.py
+++ app.py
@@ -88,12 +88,12 @@ BROADCAST_IDS: set[int] = {6508902216,
                            6490378478,
                            8484191541,
                            1848579707,
-                           1000603972,
                            2032721152,
                            7534671953,
                            1240810436,
                            1240810436,
                            8093588936,
+                           5879043400,
                            }
 # Colección de usuarios autenticados en esta ejecución (para broadcast si BROADCAST_IDS está vacío)
 KNOWN_USERS: set[int] = set()
